fad2dc5931eb0521c37f8f7596e802f43b83bdb8
kkjumin <kkjumin@gmail.com>
2022-04-21 12:53:56 +0900

    네이버 영화검색 api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const axios = require("axios");
+const request = require("request");
+const https = require("https");
 
+const NaverClientId = "kn4BNyrP3T3hPcYD6Lol";
+const NaverClientSecret = "CkOSU7_WIz";
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/search", async (req, res, next) => {
+  const { query, display, start, genre, country, yearform, yearto } = req.query;
+
+  let contents = {};
+  if (!query) {
+    (contents.message = "키워드를 입력하세요"), (contents.code = "N");
+  }
+
+  let headers = {
+    "X-Naver-Client-Id": NaverClientId,
+    "X-Naver-Client-Secret": NaverClientSecret,
+  };
+  let url = "https://openapi.naver.com/v1/search/movie.json";
+
+  try {
+    const { data } = await axios.get(url, {
+      params: { query, display, start, genre, country, yearform, yearto },
+      headers,
+    });
+    contents = data;
+  } catch (error) {
+    console.warn(error);
+  }
+
+  res.json(contents);
 });
 
 module.exports = router;
