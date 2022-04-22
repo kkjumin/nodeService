a18976294fa0750c803f0bc2715ddba8e3693caf
kkjumin <kkjumin@gmail.com>
2022-04-22 13:14:09 +0900

    boxoffice

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,14 @@ const https = require("https");
 
 const NaverClientId = "kn4BNyrP3T3hPcYD6Lol";
 const NaverClientSecret = "CkOSU7_WIz";
+const boxOfficeKey = "2489637e01ee31f9dc9182235ac7c8a2";
+const boxOfficeSubKey = "405022d93ae1097603106df9de0418b6";
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/search", async (req, res, next) => {
+router.get("/api/search", async (req, res, next) => {
   const { query, display, start, genre, country, yearform, yearto } = req.query;
 
   let contents = {};
@@ -29,6 +31,34 @@ router.get("/search", async (req, res, next) => {
     const { data } = await axios.get(url, {
       params: { query, display, start, genre, country, yearform, yearto },
       headers,
+    });
+    contents = data;
+  } catch (error) {
+    console.warn(error);
+  }
+
+  res.json(contents);
+});
+
+router.get("/api/boxOffice", async (req, res, next) => {
+  const { targetDt, itemPerPage, multiMovieYn, repNationCd, wideAreaCd } =
+    req.query;
+
+  let contents = {};
+
+  let url =
+    "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json";
+
+  try {
+    const { data } = await axios.get(url, {
+      params: {
+        key: boxOfficeKey,
+        targetDt,
+        itemPerPage,
+        multiMovieYn,
+        repNationCd,
+        wideAreaCd,
+      },
     });
     contents = data;
   } catch (error) {
